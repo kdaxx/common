2719e81275e5c336620aac22c032361c82536105
kdaxx <54764835+kdaxx@users.noreply.github.com>
2025-11-09 00:50:57 +0800

    optimize optimize opt method

--- opt/byte.go
+++ opt/byte.go
@@ -4,11 +4,26 @@ func Has(target byte, has byte) bool {
 	return (target & has) != 0
 }
 
-func Set(target byte, set byte) byte {
-	target |= set
+func SetOpt(target byte, opt byte) byte {
+	target |= opt
 	return target
 }
 func Unset(target byte, unset byte) byte {
 	target &= ^unset
 	return target
+}
+
+func SetOpts(target byte, opts ...byte) byte {
+	for i := 0; i < len(opts); i++ {
+		target |= opts[i]
+	}
+	return target
+}
+
+func Opts(opts ...byte) byte {
+	opt := byte(0)
+	for i := 0; i < len(opts); i++ {
+		opt |= opts[i]
+	}
+	return opt
 }
